cf45d8ab6b97e33a95e3f94b995cd39c5d96841e
ester miranda <esterssmiranda@gmail.com>
2025-08-18 16:15:51 -0300

    correçao do layout

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -2,8 +2,8 @@ import { Header } from "../../src/components/header"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="pt-BR">
-      <body>
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body suppressHydrationWarning>
         <Header />
         <main>{children}</main>
       </body>
